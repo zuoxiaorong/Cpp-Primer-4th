fe7986a3f54a85bcfe6a4bebec04d545bb613525
左晓荣zuoxr <zuoxr@eav.top>
2023-05-17 13:47:38 +0800

    控制流：while与for

---
+++ Exercise/Chapter01/1_4_2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+    /* 1.10
+    int sum = 0, val = 50;
+    while (val <= 100)
+    {
+        sum += val;        // assigns sum + val to sum 
+        ++val;             // add 1 to val 
+    }
+    // for (int val = 50; val <= 100; ++val)
+    //     sum += val;
+    std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+    */
+
+    /* 1.11
+    int val = 10;
+    while (val >= 0)
+    {
+        std::cout << val << std::endl;
+        --val; 
+    }
+    for (int val = 10; val >= 0; --val)
+        std::cout << val << std::endl;
+    */
+}
