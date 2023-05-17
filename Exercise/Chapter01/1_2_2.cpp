458aa0d7508d02c283f6aea45ce27b674a028134
左晓荣zuoxr <zuoxr@eav.top>
2023-05-17 13:43:21 +0800

    输入输出、初始化变量

---
+++ Exercise/Chapter01/1_2_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    // 1.3
+    std::cout << "Hello, World" << std::endl;
+
+    // 1.4
+    int a, b;
+    a = 10, b = 5;
+    std::cout << "The sum of " << a << " and " << b << " is " << a + b << std::endl;
+    std::cout << "The product of " << a << " and " << b << " is " << a * b << std::endl;
+
+    // 1.5
+    std::cout << "Hi there, I'm zxr from Suzhou" << std::endl;
+    std::cout << "Hi ";
+    std::cout << "there, ";
+    std::cout << "I'm ";
+    std::cout << "zxr ";
+    std::cout << "from ";
+    std::cout << "Suzhou" << std::endl;
+    
+    // 1.6
+    int v1 = 10, v2 = 6;
+    std::cout << "The sum of " << v1
+              << " and " << v2
+              << " is " << v1 + v2 << std::endl;
+}
